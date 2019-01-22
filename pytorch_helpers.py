fb3eeaf4d323ed8e876b278513012a900e30a3e8
Philipp Gruening <gruening@pc12.inb.uni-luebeck.de>
2019-01-22 09:59:04 +0100

    Add norm print function for pytorch model

--- pytorch_helpers.py
+++ pytorch_helpers.py
@@ -6,3 +6,12 @@ def cuda_to_numpy(x):
         return x.permute([1, 2, 0]).numpy()
     else:
         raise ValueError('Unkwnown dimensionality: {}'.format(x.dim()))
+
+
+def check_norm(model):
+    P = ctr_ = 0.0
+    for p in list(filter(lambda p: p.grad is not None, model.parameters())):
+        P += p.grad.data.norm(2).item()
+        ctr_ += 1.0
+
+    print('Mean gradient: {}'.format(P/ctr_))
