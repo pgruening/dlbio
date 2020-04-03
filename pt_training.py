898f2ea780842d5b5f94bddf555a6214406bdb1a
Philipp Gruening <gruening@pc14.inb.uni-luebeck.de>
2020-04-03 15:54:19 +0200

    add documentation

--- pt_training.py
+++ pt_training.py
@@ -70,34 +70,122 @@ def get_train_arg_parser(config):
 
 
 class ITrainInterface():
+    """
+    Train Interface handle the prediction of the network, the loss 
+    computation and the computation of additional training metrics.
+    These steps can quickly change depending on the dataset, the model
+    architecture, the task and so on. Therefore, it is reasonable to
+    create separate modules that are passed to the Training class.
+
+    You need to implement the constructor and the train_step method,
+    if the computations in the validation step differ from the train_step
+    you need to overwrite val_step
+
+    """
+
     def __init__(self, *args, **kwargs):
-        # NOTE: needs a model, a device, a loss function, and metrics
+        """Constuctor. Usually you need to provide and process:
+        - a model
+        - a device
+        - implement a loss function
+        - implement additional metrics
+        """
         raise NotImplementedError('Needs model and loss fcn and metrics')
 
     def train_step(self, *args, **kwargs):
-        # NOTE use like this:
-        # loss, metrics = self.train_interface.train_step(sample)
+        """
+        In the Training class, this functions is called for each drawn batch
+        like this:
+
+        loss, metrics = self.train_interface.train_step(sample)
+        (for more information see '_train_step' method)
+
+        Accordingly, you should compute the loss based on the prediction of
+        your model and other metrics.
+
+        The loss is used to update the weights of the model
+
+        """
         raise NotImplementedError('Implement to run training')
 
     def val_step(self, *args, **kwargs):
+        """
+        By default, the same code as in train_step is excecuted.
+        """
         # usually exactly the same as the train step
         return self.train_step(*args, **kwargs)
 
 
 class Training():
+    """Class that contains all necessary ingredients to train a pytorch
+    model. To start training simple call the instantiated object with the
+    desired number of epoch
+
+    e.g.:
+    training = Training(...)
+    training(100) # train for 100 epochs
+
+
+    """
+
     def __init__(
             self, optimizer, data_loader, train_interface,
             save_steps=-1, save_path=None,
-            metric_fcns_=[], printer=None, scheduler=None, clip=None,
+            printer=None, scheduler=None, clip=None,
             retain_graph=False, val_data_loader=None, early_stopping=None
     ):
+        """Constructor
+
+        Parameters
+        ----------
+        optimizer : pytorch optimizer
+            controls the weight updates, see get_optimizer for more information
+        data_loader : pytorch dataloader
+            when iterated over in a for loop, data are returned in batches.
+            Note that the for loop is executed as
+            'for sample in data_loader:'
+            You need to specify what sample actually is in the training-
+            interface.
+        train_interface : ITrainInterface
+            computes the loss of a batch, see method _train_step
+        save_steps : int, optional
+            every 'save_steps' the model is save to 'save_path'. If 0, the
+            model is only saved on the end of the trainig. By default -1,
+            which means the model is not saved at all (if early_stopping is 
+            None).
+        save_path : str, optional
+            where to save the model, by default None. Needs to be specified if
+            save_steps != 1
+        printer : Printer (pt_train_printer), optional
+            Prints current training values to terminal and possibly a
+            log.json file. By default None, nothing is printed or logged.
+        scheduler : pytorch scheduler, optional
+            updates the learning rate according to some schedule. By default 
+            None, no scheduling is used.
+        clip : float, optional
+            gradient clipping, by default None, no gradient clipping
+        retain_graph : bool, optional
+            needed for special backpropagation function, see pytorch 
+            documentation for more information. By default False.
+        val_data_loader : pytorch data_loader, optional
+            can be used to validation the network performance. These data are
+            not used for training (but maybe early stopping). The model is in
+            eval-mode, when those data are applied.
+            By default None, no validation is done.
+        early_stopping : EarlyStopping object, optional
+            save the model based on a specified metric, each time the best 
+            value of this metric is reached. By default None, no early stopping
+
+        Returns
+        -------
+        Training object
+        """
         self.optimizer = optimizer
         self.data_loader = data_loader
 
         assert issubclass(train_interface.__class__, ITrainInterface)
         self.train_interface = train_interface
 
-        self.metric_fcns_ = metric_fcns_
         self.scheduler = scheduler
         self.early_stopping = early_stopping
 
@@ -127,6 +215,13 @@ class Training():
             warnings.warn('No GPU detected. Training can be slow.')
 
     def __call__(self, epochs_):
+        """Train the model for a specified number of epochs
+
+        Parameters
+        ----------
+        epochs_ : int
+            how many epochs for training
+        """
         self.printer.restart()
 
         do_stop = False
@@ -163,6 +258,21 @@ class Training():
                 return
 
     def _train_step(self, sample, current_phase):
+        """Compute loss and metrics
+
+        Parameters
+        ----------
+        sample : anything provided by the data loader
+            typically the sample x and the corresponding label
+        current_phase : str
+            training or validation
+
+        Returns
+        -------
+        float, dict
+            loss value that is used for gradient computation and a dictionary
+            with metrics.
+        """
         if current_phase == 'validation':
             with torch.no_grad():
                 loss, metrics = self.train_interface.val_step(sample)
@@ -171,6 +281,13 @@ class Training():
         return loss, metrics
 
     def _update_weights(self, loss):
+        """Compute gradient and apply backpropagation
+
+        Parameters
+        ----------
+        loss : float
+            error function the weight update is based on
+        """
         self.optimizer.zero_grad()
         loss.backward(retain_graph=self.retain_graph)
 
@@ -182,6 +299,20 @@ class Training():
         self.optimizer.step()
 
     def _update_printer(self, epoch, loss, metrics, current_phase):
+        """Pass the necessary values to the printer
+
+        Parameters
+        ----------
+        epoch : int
+            current epoch
+        loss : float
+            current loss value
+        metrics : dict
+        current_phase : str
+            if the current phase is validation, all metrics/losses are renamed 
+            to val_[name]
+
+        """
         if current_phase == 'train':
             self.printer.update(loss, epoch, metrics)
         else:
@@ -192,11 +323,22 @@ class Training():
         self.printer.print_conditional()
 
     def _schedule(self, current_phase):
+        """update the scheduler after each epoch
+        """
         if self.scheduler is not None:
             if current_phase == 'train':
                 self.scheduler.step()
 
     def _save(self, epoch, epochs_):
+        """save the model to model path every 'save_steps' epochs.
+
+        Parameters
+        ----------
+        epoch : int
+            current epoch
+        epochs_ : int
+            number of epochs for entire training
+        """
         if self.do_save:
             if epoch == epochs_ - 1 or epoch % self.save_steps == 0:
                 print(f'Saving {self.save_path}')
@@ -204,6 +346,26 @@ class Training():
 
 
 def get_optimizer(opt_id, parameters, learning_rate, **kwargs):
+    """ Simple getter function for a pytorch optimizer
+
+    Parameters
+    ----------
+    opt_id : str
+        which optimizer, e.g. SGD or Adam
+    parameters : model.parameters
+        pytorch variables that shall be updated, usually model.parameters()
+        is passed
+    learning_rate : float
+
+    Returns
+    -------
+    pytorch optimizer
+
+    Raises
+    ------
+    ValueError
+        if unknown opt_id
+    """
     if opt_id == 'SGD':
         optimizer = optim.SGD(parameters,
                               lr=learning_rate,
@@ -238,6 +400,22 @@ def get_optimizer(opt_id, parameters, learning_rate, **kwargs):
 
 
 def get_scheduler(lr_steps, epochs, optimizer, gamma=.1):
+    """returns a pytorch scheduler
+
+    Parameters
+    ----------
+    lr_steps : int
+        the learning rate is altered in 'lr_steps' uniformly steps
+    epochs : int
+        number of epochs for the entire training
+    optimizer : pytroch optimizer
+    gamma : float, optional
+        the learning rate is multiplied by gamma, by default .1
+
+    Returns
+    -------
+    pytorch scheduler
+    """
     if lr_steps < 1:
         return None
 
@@ -254,12 +432,30 @@ def get_scheduler(lr_steps, epochs, optimizer, gamma=.1):
 
 
 def set_device(device=None):
+    """Use if you have multiple GPUs, but you only want to use a subset.
+    Use the command nvidia-smi in the terminal for more information on your
+    pc's gpu setup
+
+    Parameters
+    ----------
+    device : int, optional
+        masks all devices but 'device'. By default None, all devices are
+        visible
+    """
     if device is not None:
         os.environ['CUDA_VISIBLE_DEVICES'] = str(device)
         print(f'using device {device}')
 
 
 def set_random_seed(seed):
+    """Sets a seed for all training related random functions. The seed is only
+    identical on the same machine.
+
+    Parameters
+    ----------
+    seed : int
+
+    """
     print(f'Setting seed: {seed}')
 
     np.random.seed(seed)
@@ -293,6 +489,16 @@ def set_random_seed(seed):
 
 
 def loss_verification(train_interface, data_loader, printer):
+    """Run through one epoch and print the corresponding loss.
+    When using cross-entropy, the usual loss should be -ln(num_classes). If
+    not, there might be something wrong with your code.
+
+    Parameters
+    ----------
+    train_interface : ITrainInterface
+    data_loader : pytroch data_loader
+    printer : Printer (pt_train_printer.py)
+    """
     # verify loss
     print('Running loss verification')
     with torch.no_grad():
@@ -311,7 +517,41 @@ def loss_verification(train_interface, data_loader, printer):
 
 
 class EarlyStopping():
+    """Save the best model depending on a specified metric on the validation
+    set.
+
+    Returns
+    -------
+    EarlyStopping
+    """
+
     def __init__(self, metric_key, get_max=True, epoch_thres=np.inf):
+        """Costructor. You need to specify which metric should be observed,
+        if the value is better when decreased or increased.
+        For example 
+        EarlyStopping('val_acc', get_max=True, epoch_thres=10)
+        keeps track of the validation accuracy. If the current best validation
+        accuracy (starting from -inf) is exceeded, this value is saved and 
+        furthermore, the model is saved.
+
+        If after 10 epochs the best accuracy is not exceeded, the training
+        is stopped.
+
+        This object is used within the Training class.
+
+        Parameters
+        ----------
+        metric_key : str
+            Which metric is observed. Needs to be a metric that is present in
+            the training_interface. val_[name] is also possible.
+        get_max : bool, optional
+            Save the model if the new observed metric is above the current best
+            value (True) or below it (False). By default True.
+        epoch_thres : int, optional
+            if the model has not bin saved for 'epoch_thres' epochs,
+            the training is stopped. By default np.inf, the model is trained 
+            the full number of epochs.
+        """
         self.key = metric_key
         self.get_max = get_max
 
