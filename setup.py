d9f438d22ad27f7f89429769f8eb6cbd06b70fd3
Philipp Gruening <gruening@pc14.inb.uni-luebeck.de>
2020-05-11 12:28:34 +0200

    try package dir

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='DLBio',
     version='0.0.1',
-    packages=find_packages('src')
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
 )
