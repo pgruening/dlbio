deaa312628b4fad0da503cccde5a03005ba3393f
pgruening <pg@prcmail.de>
2020-09-17 16:39:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='DLBio',
     version='0.0.1',
-    packages=find_packages('src'),
+    packages=find_packages('DLBio'),
     url='https://github.com/pgruening/dlbio'
 )
