97f0d0e837e21fbb574563e80825b3ba8918e3dd
pgruening <pg@prcmail.de>
2020-09-17 23:46:21 +0200

    Update setup.py
    add dependencies

--- setup.py
+++ setup.py
@@ -4,5 +4,16 @@ setup(
     name='DLBio',
     version='0.0.1',
     packages=['DLBio'],
-    url='https://github.com/pgruening/dlbio'
+    url='https://github.com/pgruening/dlbio',
+    install_requires=[
+        'torch',
+        'torchvision',
+        'numpy',
+        'scikit-learn',
+        'tkinter',
+        'matplotlib',
+        'pandas',
+        'opencv-python',
+        'recordtype'
+      ]
 )
