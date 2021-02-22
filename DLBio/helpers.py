3d70e9c6cd5c48432a49ffe99b5df0ae4aaa3635
Philipp Gruening <gruening@pc14.inb.uni-luebeck.de>
2021-02-22 09:11:53 +0100

    BUGFIX: dataframe missing values

--- DLBio/helpers.py
+++ DLBio/helpers.py
@@ -223,8 +223,10 @@ def dict_to_options(opt_dict):
 
 
 class MyDataFrame():
-    def __init__(self):
+    def __init__(self, verbose=0):
         self.x = dict()
+        self.max_num_items = 0
+        self.verbose = verbose
 
     def update(self, in_dict, add_missing_values=False, missing_val=np.nan):
         for k, v in in_dict.items():
@@ -233,15 +235,35 @@ class MyDataFrame():
                 warnings.warn(f'Input for {k} is list, consider add_col.')
 
             if k not in list(self.x.keys()):
-                self.x[k] = [v]
+                if self.verbose > 0:
+                    print(f'added {k}')
+                # case 1: df just intialized
+                if self.max_num_items == 0:
+                    self.x[k] = [v]
+                else:
+                    # case 2: entire new key is added
+                    if add_missing_values:
+                        # fill with missing values to current num items
+                        self.x[k] = [missing_val] * self.max_num_items
+                        self.x[k].append(v)
+
             else:
                 self.x[k].append(v)
 
         if add_missing_values:
-            max_num_items = max([len(v) for v in self.x.values()])
-            for k in self.x.keys():
-                while len(self.x[k]) < max_num_items:
-                    self.x[k].append(missing_val)
+            self._add_missing(missing_val)
+
+    def _add_missing(self, missing_val):
+        self._update()
+        for k in self.x.keys():
+            if self.verbose > 1 and len(self.x[k]) < self.max_num_items:
+                print(f'add missing: {k}')
+
+            while len(self.x[k]) < self.max_num_items:
+                self.x[k].append(missing_val)
+
+    def _update(self):
+        self.max_num_items = max([len(v) for v in self.x.values()])
 
     def add_col(self, key, col):
         self.x[key] = col
